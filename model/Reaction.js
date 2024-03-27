c02823a4b7c24d9f8aed796887a80c51033b32cd
Meow Roar <sareacct91@gmail.com>
2024-03-27 14:27:38 -0700

    setup Reaction model

---
+++ model/Reaction.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Reaction extends Model { };
+
+Reaction.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  reactionBody: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 280],
+    },
+  },
+  thought_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'thought',
+      key: 'id',
+    },
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'username',
+    },
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  timestamps: true,
+  updatedAt: false,
+  createdAt: 'created_at',
+  underscored: true,
+  freezeTableName: true,
+  modelName: 'reaction'
+});
+
+
+module.exports = Reaction;
