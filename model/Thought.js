ad30eec3627616dd8eb33a7232fc45112ed51b82
Meow Roar <sareacct91@gmail.com>
2024-03-27 14:27:30 -0700

    setup Thought model

---
+++ model/Thought.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Thought extends Model { };
+
+Thought.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  thoughtText: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      len: [1, 280],
+    },
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'username',
+    },
+  },
+}, {
+  sequelize,
+  underscored: true,
+  freezeTableName: true,
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false,
+  modelName: 'thought',
+});
+
+
+module.exports = Thought;
