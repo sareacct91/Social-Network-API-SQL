0755989a5bf6f7d96467b84fdf3cdacfdee7f2fe
Meow Roar <sareacct91@gmail.com>
2024-03-27 15:05:37 -0700

    added seed route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiRouter = require('./api/');
 
+router.post('/seed', require('../seed/seed'));
 router.use('/api', apiRouter);
 
 module.exports = router;
