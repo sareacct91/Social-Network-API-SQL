533d5eb3314f105880e334e90381d8c91ac5d5bc
Meow Roar <sareacct91@gmail.com>
2024-03-28 02:33:38 -0700

    Added users routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 const c = require('../../controller/userController');
 
+router.route('/')
+  .get(c.findAllUsers)
+  .post(c.createUser)
+
+router.route('/:id')
+  .get(c.findOneUser)
+  .put(c.updateUser)
+  .delete(c.deleteUser)
+
+router.route('/:id/friends/:friendId')
+  .post(c.addFriend)
 
 module.exports = router;
