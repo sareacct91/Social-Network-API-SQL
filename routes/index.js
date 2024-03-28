36305f3c1d0e0b1b76529c7f4316123b2f3708c4
Meow Roar <sareacct91@gmail.com>
2024-03-27 17:22:21 -0700

    update seed routing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRouter = require('./api/');
 
-router.post('/seed', require('../seed/seed'));
+router.patch('/seed', require('../seed/seed'));
 router.use('/api', apiRouter);
 
 module.exports = router;
